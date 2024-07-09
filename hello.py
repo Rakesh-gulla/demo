c0ba654a81aad172ab26dab8333645745dc67064
Rakesh-gulla <gullarakesh656@gmail.com>
2024-07-09 10:19:31 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello World!!!")
+
+a = 10
+
+b = 20
+
+addit = a+b
+
+sub = b-a
+
+mul = a*b
+
+divi = b/a
+
+print('Addition is:', addit, 'Subtraction is:', sub, 'Multiplication:', mul, 'Devision is:', divi) 
